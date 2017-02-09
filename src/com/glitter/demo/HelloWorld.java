be79e22b002f155404a893f25bea7464f1cc520e
limengjun <limengjun@syswin.com>
2017-02-09 11:04:38 +0800

    测试下

--- src/com/glitter/demo/HelloWorld.java
+++ src/com/glitter/demo/HelloWorld.java
@@ -1,10 +1,14 @@
 package com.glitter.demo;
 
 /**
- * Created by Administrator on 2017/2/9.
+ * Created by Weixiaozheqingfu on 2017/2/9.
  */
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        System.out.println("打印中文吧");
     }
+
+
 }
