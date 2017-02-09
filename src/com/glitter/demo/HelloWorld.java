d1e6285ae9cdc98744669c930feff46ba2db66d8
limengjun <limengjun@syswin.com>
2017-02-09 10:08:37 +0800

    Initial commit

---
+++ src/com/glitter/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.glitter.demo;
+
+/**
+ * Created by Administrator on 2017/2/9.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
